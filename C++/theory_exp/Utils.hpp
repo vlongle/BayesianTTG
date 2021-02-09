d6eb9b09636be6c2af586f05ff959be6c3dc1931
vlongle <vietlong.lenguyen@gmail.com>
2021-02-09 15:58:55 -0500

    theory experiment counter

---
+++ C++/theory_exp/Utils.hpp
@@ -0,0 +1,212 @@
+#pragma once
+#include <Eigen/Dense>
+#include "simplex_grid.hpp"
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <utility>
+#include <tuple>
+#include <random>
+using namespace Eigen;
+using namespace std;
+// https://stackoverflow.com/questions/8833938/is-the-stdset-iteration-order-always-ascending-according-to-the-c-specificat#:~:text=Per%20the%20C%2B%2B%20standard%2C%20iteration,optional%20comparison%20predicate%20template%20argument.
+// iterating through set always maintain the same order (non-descending order)
+// which is very nice!
+typedef set<int> Coalition;    // list of agentNames
+typedef MatrixXd divisionRule; // each row is a share vector. This matrix
+// rows consist of all regular grid points on the simplex.
+// Shares are incremented in step of 10%. For example, if we have 2 players then
+// divisionRule =
+//  0   1
+// 0.1 0.9
+// 0.2 0.8
+// 0.3 0.7
+// 0.4 0.6
+// 0.5 0.5
+// 0.6 0.4
+// 0.7 0.3
+// 0.8 0.2
+// 0.9 0.1
+//   1   0
+typedef pair<Coalition, VectorXd> Proposal;
+// list of pair (coalition, divisionRule, coalitionValue
+typedef vector<tuple<Coalition, VectorXd, double>> CoalitionStructure;
+
+
+//MatrixXd softmax(const MatrixXd &M);
+
+VectorXd softmax(const VectorXd &v);
+
+MatrixXd generateDivisionRule(int numPlayers);
+
+// generate powerset for a set of integer elts
+vector<set<int>> powerSet(const vector<int> &elts);
+
+void printSet(set<int> A);
+void printVec(vector<int> A);
+void printVec(vector<double> A);
+
+// type of A, B is vector<vector<T>> instead of more natural vector<T> to
+// support CartesianSelfProduct
+template <typename T>
+vector<vector<T>> CartesianProduct(vector<vector<T>> A, vector<vector<T>> B)
+{
+    vector<vector<T>> ret;
+    for (auto &a : A)
+    {
+        for (auto &b : B)
+        {
+            vector<T> c = a; // hopefully copy by value!
+            // concatenate two vectors c and b!
+            c.insert(c.end(), b.begin(), b.end());
+            ret.push_back(c);
+        }
+    }
+    return ret;
+}
+
+template <typename T>
+// self product of A "repeat" number of times
+vector<vector<T>> CartesianSelfProduct(vector<T> A, int repeat)
+{
+
+    vector<vector<T>> A_transformed;
+    for (auto elt : A)
+    {
+        A_transformed.push_back(vector<T>({elt}));
+    }
+    vector<vector<T>> ret = A_transformed;
+    for (int i = 0; i < repeat - 1; i++)
+    {
+        ret = CartesianProduct(ret, A_transformed);
+    }
+    return ret;
+}
+
+
+
+// select an index based on probabilities given in probs. Probs should
+// already be a proper probability vector
+int selectAction(const VectorXd &probs, mt19937_64 &generator, uniform_real_distribution<double> &u);
+
+// get the index of the proposal within proposals
+int getIndex(vector<Proposal> &proposals, const Proposal &proposal);
+int getInvCount(int arr[], int n);
+
+// http://reedbeta.com/blog/python-like-enumerate-in-cpp17/
+// Python-like enumerate in C++17!!
+// need C++17 with at least g++-7 compiler!
+template <typename T,
+          typename TIter = decltype(std::begin(std::declval<T>())),
+          typename = decltype(std::end(std::declval<T>()))>
+constexpr auto enumerate(T &&iterable)
+{
+    struct iterator
+    {
+        size_t i;
+        TIter iter;
+        bool operator!=(const iterator &other) const { return iter != other.iter; }
+        void operator++()
+        {
+            ++i;
+            ++iter;
+        }
+        auto operator*() const { return std::tie(i, *iter); }
+    };
+    struct iterable_wrapper
+    {
+        T iterable;
+        auto begin() { return iterator{0, std::begin(iterable)}; }
+        auto end() { return iterator{0, std::end(iterable)}; }
+    };
+    return iterable_wrapper{std::forward<T>(iterable)};
+}
+
+// https://stackoverflow.com/questions/37368787/c-sort-one-vector-based-on-another-one
+// to sort the division rule still remembering the name of the agents involved!
+// Fill the zipped vector with pairs consisting of the
+// corresponding elements of a and b. (This assumes
+// that the vectors have equal length)
+
+// https://stackoverflow.com/questions/18973042/symbols-not-found-for-architecture-x86-64-on-qtcreator-project
+// Also, note that we cannot split template class/function into .h and .cpp parts!
+// populate zipped
+template <typename A, typename B>
+void zip(
+    pair<Coalition, VectorXd> proposal,
+    std::vector<std::pair<A, B>> &zipped)
+{
+
+    Coalition &a = proposal.first;
+    VectorXd &b = proposal.second;
+    for (auto [i, agentName] : enumerate(a))
+    {
+        zipped.push_back(std::make_pair(agentName, b[i]));
+    }
+}
+
+// populate a and b
+template <typename A, typename B>
+void unzip(
+    const std::vector<std::pair<A, B>> &zipped,
+    VectorXd &a,
+    VectorXd &b)
+{
+
+    for (size_t i = 0; i < a.size(); i++)
+    {
+        a[i] = zipped[i].first;
+        b[i] = zipped[i].second;
+    }
+}
+
+template <typename a>
+vector<a> set2vec(set<a> s)
+{
+    std::vector<a> v(s.size());
+    std::copy(s.begin(), s.end(), v.begin());
+    return v;
+}
+
+template <typename a>
+set<a> vec2set(vector<a> v)
+{
+    std::set<a> s(v.begin(), v.end());
+    return s;
+}
+
+template <typename a>
+int countInversions(vector<a> groundTruth, vector<a> candidate)
+{
+    if (groundTruth.size() != candidate.size())
+    {
+        cout << "CRITICAL!!!!! Something wrong in countInversion() function" << endl;
+        return 0;
+    }
+
+    int n = groundTruth.size();
+    int inversions = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+
+            if (groundTruth[i] > groundTruth[j] && candidate[i] <= candidate[j])
+            {
+                inversions++;
+            }
+            else if (groundTruth[i] < groundTruth[j] && candidate[i] >= candidate[j])
+            {
+                inversions++;
+            }
+            else if (groundTruth[i] == groundTruth[j] && candidate[i] != candidate[j])
+            {
+                inversions++;
+            }
+        }
+    }
+
+    return inversions;
+}
+
--- C++/theory_exp/theory.cpp
+++ C++/theory_exp/theory.cpp
@@ -6,9 +6,20 @@
 #include <algorithm>
 #include <random>
 #include <iterator>
+#include "Utils.hpp"
+#include <chrono>
 
 using namespace std;
 using namespace Eigen;
+using namespace std::chrono;
+
+// vector of tasks ordered in increasing reward and threshold
+typedef vector<pair<double, double>> Environment;
+typedef vector<int> Outcome;
+typedef vector<Outcome> Path;
+
+// roomOrientedOutcome[roomNumber] = vector of players in that room
+typedef vector<vector<int>> roomOrientedOutcome;
 
 template <typename T>
 void printVec(vector<T> &v)
@@ -19,6 +30,8 @@ void printVec(vector<T> &v)
     }
 }
 
+// // one environment
+// // NOTE: money(subset) syntax only works for the new 3.4.0 Eigen (dev not stable released) library
 void simulation(int T, mt19937 &generator, int N, string filename)
 {
     // i = -2, j = -1
@@ -44,29 +57,236 @@ void simulation(int T, mt19937 &generator, int N, string filename)
         money += weights;
         money(subset) -= weights(subset); // kinda ugly but we don't want to reward this subset twice!
 
-        // cout << ">> subset: ";
-        // printVec(subset);
-        // cout << endl;
-        // cout << "money: \n"
-        //      << money << endl;
-        result.row(t) = money;
+        //     out << result;
+        //     out.close();
+        // }
     }
-
-    // cout << "result\n"
-    //      << result << endl;
-    out << result;
-    out.close();
 }
-int main()
+
+void runStaticEnv()
 {
     int T = 1000;
     int N = 6;
     int numTrials = 1000;
 
-    #pragma omp parallel for
+#pragma omp parallel for
     for (int trial = 0; trial < numTrials; trial++)
     {
         mt19937 generator(trial);
-        simulation(T, generator, N, "theory_" + to_string(trial) + ".txt");
+        // simulation(T, generator, N, "theory_" + to_string(trial) + ".txt");
     }
+}
+
+VectorXd generatePlayers(int maxPlayers, double maxPlayerWeight, mt19937 &generator)
+{
+    uniform_int_distribution<int> distribution(1, maxPlayers);
+    int numPlayers = distribution(generator);
+
+    VectorXd weights(numPlayers);
+    uniform_real_distribution<double> u(0.0, maxPlayerWeight);
+
+    for (int i = 0; i < numPlayers; i++)
+    {
+        weights(i) = u(generator);
+    }
+    return weights;
+}
+
+Environment generateARandomEnv(int maxNumTasks, double maxReward, double maxThreshold, mt19937 &generator)
+{
+    uniform_int_distribution<int> distribution(1, maxNumTasks);
+    int numTasks = distribution(generator);
+
+    Environment env;
+    double lowerReward = 0, lowerThreshold = 0;
+    for (int i = 0; i < numTasks; i++)
+    {
+        double reward = uniform_real_distribution<double>(lowerReward, maxReward)(generator);
+        double threshold = uniform_real_distribution<double>(lowerThreshold, maxThreshold)(generator);
+        env.push_back(make_pair(threshold, reward));
+        lowerReward = reward;
+        lowerThreshold = threshold;
+    }
+    return env;
+}
+
+vector<Environment> generateDynamicEnvs(int maxRooms, int maxNumTasks, double maxReward, double maxThreshold, mt19937 &generator)
+{
+    uniform_int_distribution<int> distribution(1, maxRooms);
+    int numRooms = distribution(generator);
+
+    vector<Environment> envs;
+    for (int i = 0; i < numRooms; i++)
+    {
+        envs.push_back(generateARandomEnv(maxNumTasks, maxReward, maxThreshold, generator));
+    }
+
+    return envs;
+}
+
+vector<Path> generateAllPaths(int numRooms, int numPlayers, int numTimeSteps)
+{
+
+    vector<int> roomNames(numRooms);
+    iota(roomNames.begin(), roomNames.end(), 0);
+    vector<Outcome> outcomes = CartesianSelfProduct(roomNames, numPlayers);
+
+    vector<Path> paths = CartesianSelfProduct(outcomes, numTimeSteps);
+    return paths;
+}
+
+roomOrientedOutcome transformOutcomeToRoomOriented(Outcome &outcome, int numRoom)
+{
+    roomOrientedOutcome transformedOutcome(numRoom);
+    for (auto [playerName, roomName] : enumerate(outcome))
+    {
+        transformedOutcome[roomName].push_back(playerName);
+    }
+    return transformedOutcome;
+}
+
+VectorXd divRule(VectorXd alpha)
+{
+    return alpha.cwiseProduct(VectorXd::Constant(alpha.size(), 1.0 / alpha.sum()));
+}
+
+double evaluateWeightInEnvironment(Environment &env, double weight)
+{
+    double bestReward = 0;
+
+    for (auto &task : env)
+    {
+        double threshold = task.first;
+        double reward = task.second;
+        if (weight >= threshold)
+        {
+            bestReward = max(reward, bestReward);
+        }
+    }
+    return bestReward;
+}
+
+// TODO: check for every time as well!!
+MatrixXd evaluateAPath(Path &path, VectorXd &playerWeights, vector<Environment> &envs)
+{
+    int T = path.size();
+    MatrixXd M(T + 1, playerWeights.size());
+    M.row(0) = VectorXd::Constant(playerWeights.size(), 1);
+    //cout << "eval path " << endl;
+    for (int t = 1; t < T + 1; t++)
+    {
+        auto assignment = transformOutcomeToRoomOriented(path[t-1], envs.size());
+        for (auto [roomNum, room] : enumerate(assignment))
+        {
+            // compute v(W) and division rule!
+            double vW = evaluateWeightInEnvironment(envs[roomNum], playerWeights(room).sum());
+            VectorXd div = divRule(M.row(t - 1)(room));
+            M.row(t)(room) = M.row(t-1)(room) + div.cwiseProduct(VectorXd::Constant(room.size(), vW)).transpose();
+        }
+    }
+    //cout << "done eval path " << endl;
+    return M;
+}
+
+void bruteForceDynamicEnvSetting(VectorXd &playerWeights, vector<Environment> &envs, mt19937 &generator, int maxNumTimeSteps, int seed)
+{
+
+    // enumerate all possible paths and then calculate the expected loss and such ...
+    vector<Path> allPaths = generateAllPaths(envs.size(), playerWeights.size(), uniform_int_distribution<int>(1, maxNumTimeSteps)(generator));
+    //cout << "Number of paths: " << allPaths.size() << endl;
+
+    int n = playerWeights.size();
+    int m = envs.size();
+    map<tuple<int, int, int>, double> desiredProb; // desiredProb(i, j, t) = Pr that Mi >= Mj given wi > wj at time t
+    for (auto &path : allPaths)
+    {
+        MatrixXd M = evaluateAPath(path, playerWeights, envs);
+        //cout << "M : " << M << endl << endl;
+        //cout << M.transpose() << endl;
+
+        // saving prob to check for counter-example
+        for (int t = 1; t < path.size()+1; t++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (playerWeights(i) > playerWeights(j) && M(t, i) >= M(t, j))
+                    {
+                        // https://stackoverflow.com/questions/19354997/whats-the-easiest-way-to-get-pythons-defaultdict-behavior-in-c
+                        auto emplace_pair = desiredProb.emplace(make_tuple(i, j, t), 0);
+
+                        // ([m]^n)^T number of possible paths at time t               
+                        emplace_pair.first->second += 1.0/allPaths.size();
+                    }
+                }
+            }
+        }
+    }
+
+    for (auto elem : desiredProb)
+    {
+        if (elem.second <= 0.5)
+        {
+            cout << "FOUND COUNTER-EXAMPLE at time " << get<2>(elem.first) << " seed " << seed << " with i, j " << get<0>(elem.first) << " " << get<1>(elem.first) << " Prob: " << elem.second << endl;
+        }
+    }
+    //return desiredProb;
+}
+// trying to find a counter-example
+void runDynamicEnv()
+{
+
+    int maxPlayers = 5;
+    double maxPlayerWeight = 5.0; // playerWeight in [0, maxWeight]
+
+    // each environment is a TTG
+    int maxRooms = 5;
+    int maxNumTasks = 5;
+    double maxReward = 10.0;
+    double maxThreshold = maxPlayers * maxPlayerWeight;
+
+    int numTrials = 500;
+    int maxNumTimeSteps = 4;
+
+#pragma omp parallel for
+    for (int i = 0; i < numTrials; i++)
+    {
+        mt19937 generator(i);
+        VectorXd playerWeights = generatePlayers(maxPlayers, maxPlayerWeight, generator);
+        vector<Environment> envs = generateDynamicEnvs(maxRooms, maxNumTasks, maxReward, maxThreshold, generator);
+
+        // manual
+        //VectorXd playerWeights(3);
+        //playerWeights << 1,2,3;
+        //Environment env = {make_pair(1,1), make_pair(2, 3)};
+        //vector<Environment> envs = {env, env};
+
+        bruteForceDynamicEnvSetting(playerWeights, envs, generator, maxNumTimeSteps, i);
+    }
+}
+int main()
+{
+
+    // vector<vector<int>> outcomes = CartesianSelfProduct({1,2,3}, 2);
+
+    // for (auto & assignment : outcomes){
+    //     printVec(assignment);
+    //     cout << endl;
+    // }
+
+    // int numRooms = 3;
+    // int numPlayers = 2;
+    // int numTimeSteps = 2;
+    // auto paths = generateAllPaths(numRooms, numPlayers, numTimeSteps);
+    // cout << "number of paths: " << paths.size() << endl;
+
+
+       auto start = high_resolution_clock::now();
+    runDynamicEnv();
+
+
+
+     auto stop = high_resolution_clock::now();
+     cout << "took " << duration_cast<seconds>(stop - start).count() << " seconds " << endl;
 }
